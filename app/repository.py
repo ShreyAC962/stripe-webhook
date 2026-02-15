2210041e8a743157f00e3c7c8df49e597c495540
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-02-15 13:28:06 -0800

    Intialized respository, saved events and defined get all events

--- app/repository.py
+++ app/repository.py
@@ -1,1 +1,21 @@
 import logging
+
+from app.models import PaymentEvent
+
+logging.basicConfig(level = logging.INFO)
+
+class PaymentRepository:
+    def __init__(self):
+        self.events = {}
+        logging.info("PaymentRepository initialized")
+
+    def save_event(self, payment_event : PaymentEvent):
+        if payment_event.event_id in self.events:
+            logging.warning(f"Duplicate event detected: {payment_event.event_id}")
+            return 
+        self.events[payment_event.event_id] = payment_event
+        logging.info(f"Event saved: {payment_event.event_id}")
+    
+    def get_all_events(self):
+        return list(self.events.values())
+
