3e5dc993a07022bed901626ad3c5d1e4c31a1051
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-02-16 12:40:04 -0800

    Handled the database operations

--- app/repository.py
+++ app/repository.py
@@ -1,21 +1,24 @@
 import logging
 
-from app.schemas import PaymentEventSchema
+from sqlalchemy import IntergrityError
+from app.database import SessionLocal
+from app.models import PaymentEvent
 
-logging.basicConfig(level = logging.INFO)
+
 
 class PaymentRepository:
-    def __init__(self):
-        self.events = {}
-        logging.info("Payment Repository initialized")
-
-    def save_event(self, payment_event : PaymentEventSchema):
-        if payment_event.event_id in self.events:
-            logging.warning(f"Duplicate event detected: {payment_event.event_id}")
-            return 
-        self.events[payment_event.event_id] = payment_event
-        logging.info(f"Event saved: {payment_event.event_id}")
-    
-    def get_all_events(self):
-        return list(self.events.values())
-
+    def save(self,  event_schema):
+        session = SessionLocal() 
+        try:
+            event = PaymentEvent(
+                event_id = event_schema.event_id,
+                amount = event_schema.amount
+            )
+            session.add(event) # Add the event to the session
+            session.commit() # Commit the transaction to save it to the database
+            logging.info(f"Saved event {event_schema.event_id} to the database.")
+        except IntergrityError:
+            session.rollback() # Rollback the transaction if there's an integrity error (e.g., duplicate event_id)
+            logging.warning(f"Event {event_schema.event_id} already exists in the database. Skipping.")
+        finally:
+            session.close() # Close the session
